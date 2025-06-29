e5d8c8778044a24fa0fd0c9cf6ee462d6702ea38
pranitpatil03 <patilpranit3112@gmail.com>
2025-06-29 21:42:47 +0530

    feat(trpc): integrated trpc + react-query, wrap layout with provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import prisma from "@/lib/prisma";
+"use client";
 
-export default async function Home() {
-  const users = await prisma.user.findMany();
+import { useTRPC } from "./trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
-  return <div>{JSON.stringify(users)}</div>;
+export default function Home() {
+  const trpc = useTRPC();
+  const greeting = useQuery(trpc.hello.queryOptions({ text: "world" }));
+  if (!greeting.data) return <div>Loading...</div>;
+  return <div>{greeting.data.greeting}</div>;
 }
