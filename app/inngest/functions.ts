f3804fd6418d00bd08f79db31508e0d336d85cf3
pranitpatil03 <patilpranit3112@gmail.com>
2025-06-30 23:53:10 +0530

    feat(inngest): refactor helloWorld to use agent-kit for ai models

--- app/inngest/functions.ts
+++ app/inngest/functions.ts
@@ -1,12 +1,21 @@
+import { gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("started", "5s");
-    await step.sleep("doing-work", "15s");
-    await step.sleep("final", "7s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const supportAgent = createAgent({
+      model: gemini({ model: "gemini-1.5-flash"}),
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer. You write readable,maintainable code. You write simple next.js and react snippets.",
+    });
+
+    const { output } = await supportAgent.run(
+      `Write the follwing snippet :${event.data.value}`
+    );
+
+    return { output };
+  }
 );
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,13 @@ import { Button } from "@/components/ui/button";
 import { useTRPC } from "./trpc/client";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [value,setValue]=useState("")
+
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -17,8 +22,15 @@ export default function Home() {
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <div className="max-w-sm pb-3">
+        <Input
+          className="w-full"
+          value={value}
+          onChange={e => setValue((e.target as HTMLInputElement).value)}
+        />
+      </div>
       <Button
-        onClick={() => invoke.mutate({ text: "Pranit" })}
+        onClick={() => invoke.mutate({ value:value })}
         disabled={invoke.isPending}
       >
         Invoke Background Job
